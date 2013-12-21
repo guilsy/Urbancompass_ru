bd4b19a00be4ee447177f821be2b1245f49a8570
Nikita Zhilin <zhilin.nikita@gmail.com>
2013-12-20 21:09:15 -1000

    add Pins/users association

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,70 +1,52 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-
-  # GET /pins
-  # GET /pins.json
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @pins = Pin.all
   end
 
-  # GET /pins/1
-  # GET /pins/1.json
   def show
   end
 
-  # GET /pins/new
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build
   end
 
-  # GET /pins/1/edit
   def edit
   end
 
-  # POST /pins
-  # POST /pins.json
   def create
-    @pin = Pin.new(pin_params)
-
-    respond_to do |format|
+    @pin = current_user.pins.build(pin_params)
       if @pin.save
-        format.html { redirect_to @pin, notice: 'Pin was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @pin }
+        redirect_to @pin, notice: 'Пин был успешно создан.'
       else
-        format.html { render action: 'new' }
-        format.json { render json: @pin.errors, status: :unprocessable_entity }
+        render action: 'new'
       end
-    end
-  end
+  end  
 
-  # PATCH/PUT /pins/1
-  # PATCH/PUT /pins/1.json
   def update
-    respond_to do |format|
       if @pin.update(pin_params)
-        format.html { redirect_to @pin, notice: 'Pin was successfully updated.' }
-        format.json { head :no_content }
+        redirect_to @pin, notice: 'Пин был успешно обновлен.'
       else
-        format.html { render action: 'edit' }
-        format.json { render json: @pin.errors, status: :unprocessable_entity }
+        render action: 'edit' 
       end
-    end
   end
 
-  # DELETE /pins/1
-  # DELETE /pins/1.json
   def destroy
     @pin.destroy
-    respond_to do |format|
-      format.html { redirect_to pins_url }
-      format.json { head :no_content }
-    end
+    redirect_to pins_url
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_pin
       @pin = Pin.find(params[:id])
+    end
+
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Вы не авторизированы редактировать этот Пин" if @pin.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
