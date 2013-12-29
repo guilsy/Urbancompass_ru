9bf761a7b9d0f3eddaf67dc02a124327c323bce5
Nikita Zhilin <zhilin.nikita@gmail.com>
2013-12-29 11:52:37 -1000

    {ics are displayed on indexpage

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,5 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_many :pics
-	has_attached_file :image, :styles => {:large => "640x480>", :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :image, :styles => {:large => "640x480>", :medium => "300x300>", :thumb => "200x200>" }
 end
