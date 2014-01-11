d38ca00bfce886dae0ad4edbf53b4e1326ab5f23
Nikita Zhilin <zhilin.nikita@gmail.com>
2014-01-10 21:37:29 -1000

    Fancybox works

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,11 +18,27 @@
 //= require_tree .
 
 $(document).ready(function() {
-  $("a.fancybox").fancybox({
+  $(".fancybox").fancybox({
 		'transitionIn'	:	'elastic',
 		'transitionOut'	:	'elastic',
 		'speedIn'		:	600, 
 		'speedOut'		:	200, 
 		'overlayShow'	:	false
 	});
+
+	$(".gallery").fancybox(
+			{						
+      "padding" : 20,
+      "imageScale" : false, 
+			"zoomOpacity" : false,
+			"zoomSpeedIn" : 1000,	
+			"zoomSpeedOut" : 1000,	
+			"zoomSpeedChange" : 1000, 
+			"frameWidth" : 700,	 
+			"frameHeight" : 600, 
+			"overlayShow" : true, 
+			"overlayOpacity" : 0.8,	
+			"hideOnContentClick" :false,
+			"centerOnScroll" : false
+			});
 });
--- app/assets/javascripts/jquery.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-	jQuery(function() {
-  	$("a.fancybox").fancybox();
-	});
-});
