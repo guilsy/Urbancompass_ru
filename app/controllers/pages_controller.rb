22e2b92d2336f510d901d5ed41f504ff3a3d2aaf
Nikita Zhilin <zhilin.nikita@gmail.com>
2014-03-06 18:43:20 -0800

    Center of Moscow is displayed by default

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,6 +10,10 @@ class PagesController < ApplicationController
   end
   def googlemaps
   	@pins = Pin.all
+    @hash = Gmaps4rails.build_markers(@pins) do |pin, marker|
+      marker.lat pin.latitude
+      marker.lng pin.longitude
+    end
   end
 
 end
